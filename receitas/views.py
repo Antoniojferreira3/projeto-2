72569c27de782722fc3cba38e81af010317510a6
antonio <cursosenai904@gmail.com>
2025-08-19 21:09:24 -0300

    segundo commit 19-08

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render 
+from django.http import Http404
  
 # receitas/views.py 
 def home(request): 
+
     return render(request, 'receitas/home.html') 
+
+def receita_detail(request, id):
+
+    context = {
+        'receita_id': id,
+        'receita_title': f'Receita Detalhada {id}', # Exemplo de título
+        'receita_description': f'Esta é a descrição detalhada da receita com ID {id}.'
+    }
