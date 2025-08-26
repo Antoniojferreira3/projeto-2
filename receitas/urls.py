0a8ff1633179667f75c7a425a37965481f2b8bc2
antonio <cursosenai904@gmail.com>
2025-08-26 19:13:43 -0300

    commit final

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,6 +1,8 @@
-# receitas/urls.py 
-from django.urls import path 
-from . import views 
-urlpatterns = [ 
-path('', views.home, name='home'), 
-] 
+# receitas/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('receita/<int:id>/', views.receita_detail, name='receita_detail'),
+]
